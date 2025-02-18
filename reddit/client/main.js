8ba7946eaba1836797ee0013ba894b1cf10c3828
Michael Whitney <whitneym@winthrop.edu>
2025-02-17 23:38:26 -0500

    04b - react and jsx intro

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -1,19 +1,33 @@
-console.log('Log from /client/main.js');
+import React from 'react'; // specify the module and then specify the library name
+                            // meteor takes care of the rest
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor'; // named export from Meteor
+  // so far we have 3 ways to import
+  // 1. relative paths './../imports/file';
+  // 2. npm libraries by providing the name 'react-dom'
+  // 3. meteor modules 'meteor/meteor' - the meteor/ removes name conflicts
 
-// we previously had the following named imports
-// import { say_hello, name } from './../imports/utils.js';
-// in order to import a default we will have to add something before the named imports
-import any_name_default_var, {say_hello, name} from './../imports/utils.js'; // multiple named imports
-// we can rename this default to anything we want. Use something that makes sense
+// Meteor.startup waits for the dom to be renderded before doing anything
+Meteor.startup(function () {  // this takes a function as its one and only argument
+  // we now need to render jsx which is javascript xml
+  //let jsx = <p>this is from client/main.js</p>
+  let name = 'newman';
+  let jsx = <p>hello {name}!</p>;
 
-console.log(any_name_default_var);
+  // the following causes problems b/c it violates the rule that you are
+  //    only allowed to have one root element
+  //let jsx = <p>this is from client/main.js</p><p>hello {name}!</p>;
+  // instead, we need to wrap it in a single root element i.e., <div>
+  jsx = <div><p>this is from client/main.js</p><p>hello {name}!</p></div>;
+  // the previous works but it is not formatted all that well. Instead:
+  jsx = (
+   <div>
+     <p>this is from client/main.js</p>
+     <p>hello {name}!</p>
+   </div>
+  );
 
-// we are not required to import anything - we can just import the default
-// import defaultVariableNameYouChoose from './../imports/utils.js';
-// console.log(defaultVariableNameYouChoose);
+  ReactDOM.render(jsx, document.getElementById('content'));
 
 
-/*
-console.log( say_hello() );
-console.log( name );
-*/
+});
