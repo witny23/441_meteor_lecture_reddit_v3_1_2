7b294062ea4db0a71c8c9700477475d5d128acb0
Michael Whitney <whitneym@winthrop.edu>
2025-02-24 11:29:26 -0500

    05a - render static data from an array object

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -1,31 +1,51 @@
-import React from 'react'; // specify the module and then specify the library name
-                            // meteor takes care of the rest
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {Meteor} from 'meteor/meteor'; // named export from Meteor
-  // so far we have 3 ways to import
-  // 1. relative paths './../imports/file';
-  // 2. npm libraries by providing the name 'react-dom'
-  // 3. meteor modules 'meteor/meteor' - the meteor/ removes name conflicts
+import {Meteor} from 'meteor/meteor';
 
-// Meteor.startup waits for the dom to be renderded before doing anything
-Meteor.startup(function () {  // this takes a function as its one and only argument
-  // we now need to render jsx which is javascript xml
-  //let jsx = <p>this is from client/main.js</p>
-  let name = 'newman';
-  let jsx = <p>hello {name}!</p>;
+Meteor.startup(function () {
 
-  // the following causes problems b/c it violates the rule that you are
-  //    only allowed to have one root element
-  //let jsx = <p>this is from client/main.js</p><p>hello {name}!</p>;
-  // instead, we need to wrap it in a single root element i.e., <div>
-  jsx = <div><p>this is from client/main.js</p><p>hello {name}!</p></div>;
-  // the previous works but it is not formatted all that well. Instead:
-  jsx = (
-   <div>
-     <p>this is from client/main.js</p>
-     <p>hello {name}!</p>
-   </div>
-  );
+  // basic function that does not accept any parameters
+  const renderSomeParagraphs = function () {
+    // return [<p>p 1</p>,<p>p 2</p>,<p>p 3</p>];
+    return [<p key='1'>p 1</p>,<p key='2'>p 2</p>,<p key='3'>p 3</p>];
+  };
+
+  const renderArrayMap = function () {
+    let numbers = [{val: 1}, {val: 2},{val: 3}];
+    // the following is an array map which is an array method
+      // basically, it is going to process the numbers array
+      // one at a time - each array entry (e.g., val:1) is assigned to number
+    let newNumbers = numbers.map(function(number){
+      return number.val - 1;
+    });
+    console.log(newNumbers);
+  };
+
+  const renderPosts = function (passed_posts) {
+    console.log(passed_posts);
+    let numbers = [{val: 11}, {val: 23},{val: 37}];
+    let newNumbers = numbers.map(function(number){
+      return <p key={number.val}>{number.val}</p>;
+    });
+
+    return newNumbers;
+  };
+
+  let title = '441 reddit';
+  let jsx = (
+         <div>
+            <h1>{title}</h1>
+            {/* the following is static but in the future it will be dynamic b/c it will come from db */}
+            {/* [<p>p 1</p>,<p>p 2</p>,<p>p 3</p>] */}
+            {/* notice the previous gives us a warning in the console
+                - this is b/c each child in the array should have a unique prop */}
+            {/* [<p key='1'>p 1</p>,<p key='2'>p 2</p>,<p key='3'>p 3</p>] */}
+
+            {renderSomeParagraphs()}
+            {renderArrayMap()}
+            {renderPosts('hi')}
+         </div>
+        );
 
   ReactDOM.render(jsx, document.getElementById('content'));
 
