941b7c839a01e9452cc2e1c58fe8c3624e9ff300
Michael Whitney <whitneym@winthrop.edu>
2025-03-02 21:07:04 -0500

    06a - basic mongoDB setup / communication

--- reddit/server/main.js
+++ reddit/server/main.js
@@ -6,7 +6,7 @@ import {UP_Collection_Access} from './../imports/api/user_posts.js';
 // this gives us access to the UP_Collection_Access object so we can interact with the DB
 
 
-Meteor.startup(function(){
+Meteor.startup(async function(){
     UP_Collection_Access.insertAsync({
       topic: 'dog',
       votes: 9,
@@ -17,7 +17,8 @@ Meteor.startup(function(){
       votes: 93,
     });
 
-  console.log(UP_Collection_Access.find().fetch());
+  console.log(await UP_Collection_Access.find().fetch());
+  // await ensures each insertAsync() completes before moving to the next line.
   // .find() returns everything
   // .fetch() is a pointer to some documents in the DB
   // to get an array of the documents you use .fetch()
