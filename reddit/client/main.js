45bde76f576566f494e733c5ba6031657afb62cc
Michael Whitney <whitneym@winthrop.edu>
2025-03-04 22:41:40 -0500

    07b - challenge: replace static array with miniMongo data

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -2,6 +2,12 @@ import React from 'react'; // specify the module and then specify the library na
                             // meteor takes care of the rest
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor'; // named export from Meteor
+import {UP_Collection_Access} from './../imports/api/user_posts.js';
+
+// Meteor.subscribe is a client-side function that tells the server to send specific data to the client. 
+// It is used to access collections that were published on the server.
+Meteor.subscribe("user_posts_collection");
+
 
 
 const renderPosts = function (passed_posts) {
@@ -14,30 +20,21 @@ const renderPosts = function (passed_posts) {
 };
 
 Meteor.startup(async function () { // async function: Marks a function as asynchronous. see server/main.js for more info 
-  const posts =[{
-        _id: '01',
-        topic: 'cats',
-        votes: 5,
-      }, {
-        _id: '02',
-        topic: 'dogs',
-        votes: 2,
-      }, {
-        _id: '03',
-        topic: 'birds',
-        votes: 11,
-      }
-  ];
-  let title = '441 reddit';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
+  // Tracker tracks queries and reruns code when queries change
+  Tracker.autorun(function(){
+    const posts = UP_Collection_Access.find().fetch();
+    let title = '441 reddit';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        {renderPosts(posts)}
+      </div>
+    );
 
-      {/* renderPosts('hi') */}
-      {renderPosts(posts)}
-    </div>
-  );
+    ReactDOM.render(jsx, document.getElementById('content'));
 
-  ReactDOM.render(jsx, document.getElementById('content'));
+  });
 
 });
+
+  
