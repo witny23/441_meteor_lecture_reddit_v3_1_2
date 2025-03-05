90622cfdc32fe4836cce3194dfec6968eff82fb4
Michael Whitney <whitneym@winthrop.edu>
2025-03-04 21:51:18 -0500

    06a - basic mongoDB setup / communication

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -2,29 +2,7 @@ import React from 'react'; // specify the module and then specify the library na
                             // meteor takes care of the rest
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor'; // named export from Meteor
-import {UP_Collection_Access} from './../imports/api/user_posts.js';
 
-// Meteor.subscribe is a client-side function that tells the server to send specific data to the client. 
-// It is used to access collections that were published on the server.
-Meteor.subscribe("user_posts_collection");
-
-
-// the following is an empty array b/c DDP has not synched up with the two DB
-console.log('Postings 1', UP_Collection_Access.find().fetch());
-
-// hack fix - set a timeout
-setTimeout(function(){
-  console.log('Posting 2', UP_Collection_Access.find().fetch());
-}, 1000);
-// this is a bad solution because it only fires once
-// if the data updates we wont see the update
-
-
-// it is better to use a built in meteor function called Tracker
-// Tracker tracks queries and reruns code when queries change
-Tracker.autorun(function(){
-  console.log('Posting 3', UP_Collection_Access.find().fetch());
-});
 
 const renderPosts = function (passed_posts) {
   console.log(passed_posts);
--- reddit/server/main.js
+++ reddit/server/main.js
@@ -5,11 +5,6 @@ import {Meteor} from 'meteor/meteor';
 import {UP_Collection_Access} from './../imports/api/user_posts.js';
 // this gives us access to the UP_Collection_Access object so we can interact with the DB
 
-// Meteor.publish() is a server-side function that controls which data is sent to the client. 
-// It allows the client to subscribe to specific datasets from the server.
-Meteor.publish("user_posts_collection", function() {
-  return UP_Collection_Access.find();
-});
 
 // promise: an object that represents the eventual completion or failure of an asynchronous operation and its resulting value.
 // async function: Marks a function as asynchronous, allowing the use of 'await' inside it which will make the function "pause" until the promise is resolved or rejected.
