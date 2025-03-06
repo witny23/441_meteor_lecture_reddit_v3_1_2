b36b8489c0e325e8959c8e221f8e3c790aaa3203
Michael Whitney <whitneym@winthrop.edu>
2025-03-05 21:40:19 -0500

    07b - challenge: replace static array with miniMongo data

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -9,22 +9,6 @@ import {UP_Collection_Access} from './../imports/api/user_posts.js';
 Meteor.subscribe("user_posts_collection");
 
 
-// the following is an empty array b/c DDP has not synched up with the two DB
-console.log('Postings 1', UP_Collection_Access.find().fetch());
-
-// hack fix - set a timeout
-setTimeout(function(){
-  console.log('Posting 2', UP_Collection_Access.find().fetch());
-}, 1000);
-// this is a bad solution because it only fires once
-// if the data updates we wont see the update
-
-
-// it is better to use a built in meteor function called Tracker
-// Tracker tracks queries and reruns code when queries change
-Tracker.autorun(function(){
-  console.log('Posting 3', UP_Collection_Access.find().fetch());
-});
 
 const renderPosts = function (passed_posts) {
   console.log(passed_posts);
@@ -35,31 +19,22 @@ const renderPosts = function (passed_posts) {
   return formattedPosts;
 };
 
-Meteor.startup(function () {
-  const posts =[{
-        _id: '01',
-        topic: 'cats',
-        votes: 5,
-      }, {
-        _id: '02',
-        topic: 'dogs',
-        votes: 2,
-      }, {
-        _id: '03',
-        topic: 'birds',
-        votes: 11,
-      }
-  ];
-  let title = '441 reddit';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
+Meteor.startup(function () { 
+  // Tracker tracks queries and reruns code when queries change
+  Tracker.autorun(function(){
+    const posts = UP_Collection_Access.find().fetch();
+    let title = '441 reddit';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        {renderPosts(posts)}
+      </div>
+    );
 
-      {/* renderPosts('hi') */}
-      {renderPosts(posts)}
-    </div>
-  );
+    ReactDOM.render(jsx, document.getElementById('content'));
 
-  ReactDOM.render(jsx, document.getElementById('content'));
+  });
 
 });
+
+  
