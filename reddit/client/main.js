e58db4a0bc8565b0cfb492f8836a94fa15f63d5c
Michael Whitney <whitneym@winthrop.edu>
2025-03-07 20:34:19 -0500

    12a - mongoDB update operators $inc, $set

--- reddit/client/main.js
+++ reddit/client/main.js
@@ -11,15 +11,20 @@ Meteor.subscribe("user_posts_collection");
 
 
 const renderPosts =  (passed_posts) =>  {
-  //console.log(passed_posts);
   let formattedPosts = passed_posts.map((post) => {
     return (
       <p key={post._id}>
-      {post.topic} have {post.votes} vote[s] {''/* single space before button hack */}
-      {/* below is a statement function */}
-      <button onClick={() => {  // nice anonymous arrow function
-        UP_Collection_Access.remove({_id: post._id})
-      }}>X</button>
+        {post.topic} have {post.votes} vote[s] {''/* single space before button hack */}
+        {/* below is a statement function */}
+        <button onClick={() => {  // anonymous arrow function
+          UP_Collection_Access.update({_id: post._id}, {$inc: {votes: 1}})
+        }}>+1</button>
+        <button onClick={() => {
+          UP_Collection_Access.update({_id: post._id}, {$inc: {votes: -1}})
+        }}>-1</button>
+        <button onClick={() => {
+          UP_Collection_Access.remove({_id: post._id})
+        }}>X</button>
       </p>
     );
   });
