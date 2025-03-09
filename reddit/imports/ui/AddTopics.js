c841d5c27a697e3f39157be27a9ea3f9232d8cad
Michael Whitney <whitneym@winthrop.edu>
2025-03-09 13:32:37 -0400

    17b - React.Component - AddTopic bind form onSubmit to event

--- reddit/imports/ui/AddTopics.js
+++ reddit/imports/ui/AddTopics.js
@@ -20,7 +20,7 @@ export default class AddTopics extends React.Component{
   render(){
     return (
       <>
-        <form onSubmit={this.processFormData}>
+        <form onSubmit={this.processFormData.bind(this)}>
           <input type='text' name='formInputNameAttribute' placeholder='Topic Name'/>
           <button>Add Topic</button>
         </form>
