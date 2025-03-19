56558b1c742536b64fb4b053bc7700bec42f6478
Michael Whitney <whitneym@winthrop.edu>
2025-03-19 16:31:42 -0400

    24a - scss style header

--- reddit/imports/ui/TitleBar.js
+++ reddit/imports/ui/TitleBar.js
@@ -12,9 +12,11 @@ export default class TitleBar extends React.Component{
   }
   render(){
     return (
-      <div>
-        <h1>{this.props.title}</h1>
-        {this.renderModerator()}
+      <div className='wrapper'>
+        <div className='title-bar'>
+          <h1>{this.props.title}</h1>
+          {this.renderModerator()}
+        </div>
       </div>
     );
   }
